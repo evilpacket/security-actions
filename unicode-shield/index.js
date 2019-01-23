3525401826284ff3497f56f2fa55fbc2de4cea4c
Adam Baldwin <adam@npmjs.com>
2019-01-23 15:29:31 -0800

    stuff and things

--- unicode-shield/index.js
+++ unicode-shield/index.js
@@ -5,8 +5,11 @@ const tools = new Toolkit()
 
 const { event, payload } = tools.context
 
+/*
 if (event === 'push') {
-  console.log(JSON.stringify(payload))
   const octokit = tools.createOctokit()
   //const result = await octokit.repos.getContents({owner, repo, path, ref})  
 }
+*/
+
+console.log(JSON.stringify(payload))
